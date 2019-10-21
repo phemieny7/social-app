23a4c1893289dda5eaf58b2a29f5eee202ef491b
Perfcreg <perfcreg@gmail.com>
2019-10-21 18:01:02 +0100

    updated

--- api/User.js
+++ api/User.js
@@ -242,8 +242,7 @@ router.post("/login", async (req, res) => {
           user: {
             id: user.id,
             name: user.name,
-            email: user.email,
-            token: token
+            email: user.email
           }
         });
       }
